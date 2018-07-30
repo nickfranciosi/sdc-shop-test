ddb2f305c0185bea8952cbcef088feca2d856307
Nick Franciosi <nick.franciosi@smiledirectclub.com>
2018-07-30 16:46:33 -0500

    update: add ajax cart actions

--- src/assets/scripts/layout/theme.js
+++ src/assets/scripts/layout/theme.js
@@ -11,6 +11,7 @@ import '../../styles/theme.scss.liquid';
 import $ from 'jquery';
 import {pageLinkFocus} from '@shopify/theme-a11y';
 import {cookiesEnabled} from '@shopify/theme-cart';
+import {formatMoney} from '@shopify/theme-currency';
 import {wrapTable, wrapIframe} from '@shopify/theme-rte';
 
 window.slate = window.slate || {};
@@ -61,38 +62,163 @@ $(document).ready(() => {
   }
 });
 
+
+// Modal Actions
+
+function hideMobileMenu() {
+  $('#mobile-menu').removeClass('open');
+  $('body').removeClass('modalOpen');
+  $('.hamburger').removeClass('open');
+}
+
+function toggleCart() {
+  // close mobile menu if open
+  hideMobileMenu();
+  $('body').toggleClass('modalDesktop');
+  $('#modalCart').toggleClass('isOpen');
+}
+
+function toggleMobileMenu() {
+  $('body').toggleClass('modalOpen');
+  $('.hamburger').toggleClass('open');
+  $('#mobile-menu').toggleClass('open');
+}
+
 $(document).ready(() => {
   $('.hamburger').on('click', (e) => {
-    $('body').toggleClass('modalOpen');
-    $('.hamburger').toggleClass('open');
-    $('#mobile-menu').toggleClass('open');
+    toggleMobileMenu();
   });
 });
-
 
 $(document).ready(() => {
 
   $('.modalCartTrigger').on('click', (event) => {
     event.preventDefault();
-    const $cartModal = $('#modalCart');
-    const $body = $('body');
-    // close mobile menu if open
-    $('#mobile-menu').removeClass('open');
-    $body.removeClass('modalOpen');
-    $('.hamburger').removeClass('open');
-
-    $body.toggleClass('modalDesktop');
-    $cartModal.toggleClass('isOpen');
+    toggleCart();
   });
 
-  $('.cart-item--incrementer button').on('click', (e) => {
+});
+
+// Product Cart actions
+
+function addToCartFail(error) {
+  console.log('fail', error);
+}
+
+function updateExistingCartItem(id, quantity) {
+  $(`#product-${id} [data-product-quantity]`).text(quantity);
+  // console.log($(`#product-${id} .cart-item--incrementer button`));
+  $(`#product-${id} .cart-item--incrementer button`).each(function() {
+    $(this).data('productCurrentQuantity', quantity);
+  });
+}
+
+function addNewCartItem(product) {
+  const prodTemplate = `
+    <div class="cart-item" id="product-${product.id}">
+      <img src="${product.image}" alt="${product.title}" />
+      <div class="cart-item--content">
+        <div class="cart-item--info">
+          <h4>${product.product_title}</h4>
+          <p>${formatMoney(product.price)}</p>
+        </div>
+        <div class="cart-item--incrementer">
+          <button
+            class="button button--text button--no-padding"
+            data-product-id="${product.id}"
+            data-product-current-quantity="${product.quantity}"
+            data-product-increment-type="decrement"
+          >
+            -
+          </button>
+          <p data-product-quantity>${product.quantity}</p>
+          <button
+            class="button button--text button--no-padding"
+            data-product-id="${product.id}"
+            data-product-current-quantity="${product.quantity}"
+            data-product-increment-type="increment"
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
+  $('.cart-items').prepend(prodTemplate);
+}
+
+function syncCartItems(product) {
+  if ($(`#product-${product.id}`).length) {
+    console.log('item exists, update values');
+    updateExistingCartItem(product.id, product.quantity);
+  } else {
+    addNewCartItem(product);
+  }
+}
+
+function addToCartSuccess(product) {
+  console.log('success', product);
+  updateCartInfo();
+  syncCartItems(product);
+  toggleCart();
+}
+
+function updateCartSuccess(cart) {
+  console.log('cart success', cart);
+  updateCartInfo();
+}
+
+function cartFetchSuccess(cart) {
+  console.log({cart});
+  $('.cart-subtotal p:last-of-type').text(formatMoney(cart.total_price));
+  $('.cart-count-wrapper').text(`(${cart.item_count})`);
+}
+
+function updateCartInfo() {
+  $.ajax({
+    type: 'GET',
+    url: '/cart.js',
+    dataType: 'json',
+    success: cartFetchSuccess,
+    error: (error) => console.log({error}),
+  });
+}
+
+$(document).ready(() => {
+  $('#product-form').on('submit', (e) => {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '/cart/add.js',
+      dataType: 'json',
+      data: $('#product-form').serialize(),
+      success: addToCartSuccess,
+      error: addToCartFail,
+    });
+  });
+
+  $('.cart-items').on('click', '.cart-item--incrementer button', (e) => {
     e.preventDefault();
     const $item = $(e.target);
     const $itemData = $item.data();
-    $.post('/cart/add.js', {
-      quantity: 1,
+    console.log($itemData);
+    const updatedQuantity = $itemData.productIncrementType === 'increment' ? $itemData.productCurrentQuantity + 1 : $itemData.productCurrentQuantity - 1;
+
+    const data = {
+      quantity: updatedQuantity,
       id: $itemData.productId,
-    });
+    };
+    if (updatedQuantity >= 1) {
+      updateExistingCartItem($itemData.productId, updatedQuantity);
+      $.ajax({
+        type: 'POST',
+        url: '/cart/change.js',
+        dataType: 'json',
+        data,
+        success: updateCartSuccess,
+        error: addToCartFail,
+      });
+    }
   });
 });
 
