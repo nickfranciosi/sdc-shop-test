af6ca658959c9c14ee11e150fa0e7f608cb23b21
Nick Franciosi <nick.franciosi@smiledirectclub.com>
2018-08-06 15:15:51 -0500

    fix: scope --vh to modal

--- src/assets/scripts/layout/theme.js
+++ src/assets/scripts/layout/theme.js
@@ -295,7 +295,8 @@ $(document).ready(() => {
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 const vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+const $modalWrapper = document.querySelector('#modalCart');
+$modalWrapper.style.setProperty('--vh', `${vh}px`);
 
 
 (function() {
@@ -319,7 +320,8 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     const vh = window.innerHeight * 0.01;
     // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    const $modalWrapper = document.querySelector('#modalCart');
+    $modalWrapper.style.setProperty('--vh', `${vh}px`);
   }
 
 })();
